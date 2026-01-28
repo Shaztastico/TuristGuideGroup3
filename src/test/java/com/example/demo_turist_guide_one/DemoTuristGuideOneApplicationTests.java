6d7a7975d9ece88434d19e1e9c012ae14396a79b
svanholm200-debug <svanholm200@gmail.com>
2026-01-28 09:43:23 +0100

    TuristGuideOne

---
+++ src/test/java/com/example/demo_turist_guide_one/DemoTuristGuideOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_turist_guide_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTuristGuideOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
